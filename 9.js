bbd676ce1f60d91fe6252cb4fef84e300c171f36
mudasser <khajashi@gmail.com>
2025-02-15 23:56:20 +0530

    solved more problems

---
+++ 9.js
@@ -0,0 +1,12 @@
+
+
+// factorial problem
+
+
+function findFactorial(number) {
+    if (number === 0) return 1; // Base case
+
+    return number * findFactorial(number - 1); // Multiply with recursive call
+}
+
+console.log(findFactorial(5)); // Output: 120
