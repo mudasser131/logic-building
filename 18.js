4fdd02790d49a800dec3f3ca0cd87d8968d39003
mudasser <khajashija@gmail.com>
2025-03-12 20:29:49 +0530

    solved one more problem

---
+++ 18.js
@@ -0,0 +1,31 @@
+
+
+// find the no if the no is positive or negative
+
+
+function   what (num){
+
+if (num < 0 ){
+
+
+    console.log('it is negative no');
+    
+}
+else if (num > 0){
+
+
+    console.log('it is a positive num');
+    
+}
+else {
+
+console.log('it is zero');
+
+
+}
+
+}
+
+what (3)
+what (0)
+what (-3)
